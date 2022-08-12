eaf072f59e991d3d772518514985ffbaf1e771fe
Ivan Escribano <ivanescribano1998@gmail.com>
2022-08-12 08:48:25 +0200

    added mvp

--- src/pages/home/components/CoverDescrtiption.jsx
+++ src/pages/home/components/CoverDescrtiption.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const CoverDescrtiption = () => {
   return (
     <div className="cover__description d-flex flex-column justify-content-center mb-5">
-      <h1 className="w-75">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quasi a
-        dicta accusamus.
+      <h1 style={{ width: "62%" }}>
+        At <span className="fw-bolder text-danger">GoTech</span> , we take pride
+        in our work and enjoy the satisfaction of a job well done
       </h1>
       <div>
         <button className="btn btn-color my-3">
